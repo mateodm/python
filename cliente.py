cc04b35c26d123610bb108178a6aa5babc816de0
Mateo DM <104037425+mateodm@users.noreply.github.com>
2023-12-21 18:36:56 -0300

    final

--- cliente.py
+++ cliente.py
@@ -1,13 +1,72 @@
+import json
+
 class Cliente:
     def __init__(self, nombre, email, direccion, telefono):
         self.nombre = nombre
         self.email = email
         self.direccion = direccion
         self.telefono = telefono
-    def registrar_cliente(self, nombre, email, direccion, telefono):
-        self.nombre = nombre
-        self.email = email
-        self.direccion = direccion
-        self.telefono = telefono
+    
+    def editar_cliente(self, nombre_buscar, nuevo_nombre=None, nuevo_email=None, nueva_direccion=None, nuevo_telefono=None):
+        # se busca por nombre, hay que establecer nuevo_NOMBRE para indicar que hay que editar
+        # editar el archivo con el cliente
+        archivo_json = "clientes.json"
+        try:
+            clientes = []
+            try:
+                with open(archivo_json, 'r') as file:
+                    clientes = json.load(file)
+                    if not isinstance(clientes, list):
+                        clientes = []
+            except FileNotFoundError:
+                pass
+
+            cliente_encontrado = None
+            for cliente in clientes:
+                if cliente.get('nombre') == nombre_buscar:
+                    cliente_encontrado = cliente
+                    break
+            
+            if cliente_encontrado:
+                if nuevo_nombre:
+                    cliente_encontrado['nombre'] = nuevo_nombre
+                if nuevo_email:
+                    cliente_encontrado['email'] = nuevo_email
+                if nueva_direccion:
+                    cliente_encontrado['direccion'] = nueva_direccion
+                if nuevo_telefono:
+                    cliente_encontrado['telefono'] = nuevo_telefono
+
+                with open(archivo_json, 'w') as file:
+                    json.dump(clientes, file, indent=2)
+                
+                return f"Cliente {nombre_buscar} modificado correctamente en {archivo_json}"
+            else:
+                return f"No se encontró un cliente con el nombre {nombre_buscar}"
+
+        except Exception as e:
+            print(f"Error al escribir en el archivo: {e}")
+            return "Error al modificar el cliente"
+    
     def __str__(self):
-        return f"Cliente: {self.nombre}, Email: {self.email}, Dirección: {self.direccion}, Teléfono: {self.telefono}"
+        archivo_json = "clientes.json"
+        try:
+            clientes = []
+            try:
+                with open(archivo_json, 'r') as file:
+                    clientes = json.load(file)
+                    if not isinstance(clientes, list):
+                            clientes = []
+            except FileNotFoundError:
+                pass
+            clientes.append(self.__dict__)
+
+            with open(archivo_json, 'w') as file:
+                json.dump(clientes, file, indent=2)
+        
+            return super().__str__()  # Devolver la representación en cadena del objeto
+        except Exception as e:
+            print(f"Error al escribir en el archivo: {e}")
+            return "Error al guardar el cliente"
+
+# crear un cliente
---
+++ gestor.py
@@ -0,0 +1,11 @@
+from cliente import Cliente
+
+# crear cliente:
+
+cliente1 = Cliente("Mateo30", "mateo40@gmail.com", "calle 662", "13233231")
+print(cliente1)
+
+# editar cliente:
+
+# cliente1.editar_cliente("Mateo8", nuevo_email="mateo25@gmail.com", nuevo_telefono="987654321")
+# print(cliente1)
